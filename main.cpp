c4ad4c2419bb403caf6178bb8679b3fe36440283
Flavio Coutinho <fegemo@gmail.com>
2016-03-01 16:09:20 -0300

    Fixes exceptions for malformed regedit entries

--- main.cpp
+++ main.cpp
@@ -10,12 +10,25 @@ string removeProtocolFromURI(string uri, string protocol) {
 int main(int argc, char** argv)
 {
 
-    string codeBlocks = string("C:\\Program Files (x86)\\CodeBlocks\\codeblocks.exe");
-    string project = removeProtocolFromURI(string(argv[1]), "codeblocks");
-    string examplesPath = string("C:\\Users\\Flavio\\Documents\\GitHub\\cefet-cg-exemplos-opengl\\");
-    string command = string("start \"") + codeBlocks + "\" \"" + examplesPath + project + "\"";
-
     cout << "Abrindo projeto no CodeBlocks..." << endl;
+
+    string codeBlocks = string("C:\\Program Files (x86)\\CodeBlocks\\codeblocks.exe");
+    cout << "\t- caminho para CodeBlocks: " << codeBlocks << endl;
+
+    if (argc < 2) {
+        cout << endl << "Programa executado com apenas " << argc << " argumento(s). Necessario pelo menos 2." << endl << endl << endl;
+        system("pause");
+        return -1;
+    }
+    string project = removeProtocolFromURI(string(argv[1]), "codeblocks");
+    cout << "\t- nome do projeto: " << project << endl;
+
+    string examplesPath = string("C:\\Users\\Flavio\\Documents\\GitHub\\cefet-cg-exemplos-opengl\\");
+    cout << "\t- pasta com exemplos: " << examplesPath << endl;
+
+    string command = string("start \"") + codeBlocks + "\" \"" + examplesPath + project + "\"";
+    cout << "\t- comando: " << command << endl;
+
     cout << "  " << examplesPath << project << endl << endl;
 
     system(command.c_str());
